16b63425d95a488d06d865df625ffc1e7912ed65
cypher3215 <140287543+cypher3215@users.noreply.github.com>
2026-02-12 04:39:47 -0500

    Add initial HTML structure for index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Clínica - Sistema</title>
+</head>
+<body>
+    <h1>Gestión Clínica</h1>
+    <ul>
+        <li><a href="pacientes.php">Gestionar Pacientes</a></li>
+        <li><a href="medicamentos.php">Gestionar Medicamentos</a></li>
+        <li><a href="asignar.php">Asignar Medicamentos</a></li>
+    </ul>
+</body>
+</html>
